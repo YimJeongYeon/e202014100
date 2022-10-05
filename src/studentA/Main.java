d754b0e3075e8cc836e9b5fbb9f44e8bca128c5a
Yim, JeongYeon <yjy5923@gmail.com>
2022-10-06 00:43:06 +0900

    Main.java 수정#4

--- src/studentA/Main.java
+++ src/studentA/Main.java
@@ -1,12 +1,14 @@
 package studentA;
 
 public class Main {
+
+    static void say() {
+        System.out.println("main");
+    }
     
     public static void main(String[] args) {
+        say();
         Hello hello = new Hello();
         hello.say("hello world");
     }
 }
-
-
-
